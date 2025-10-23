e1a2eb49ce0a34c6a058f8868d07157e9a5d3325
Redrexy <arpansubba2002@gmail.com>
2025-10-23 17:54:21 -0400

    final setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,28 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/react-in-jsx-scope": "off",
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
